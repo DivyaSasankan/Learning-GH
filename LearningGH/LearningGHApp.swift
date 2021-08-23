e5ab858ddd9b29cedfda7cd94da4c030ad0ad5fc
DivyaSasankan <divyasasankan@gmail.com>
2021-08-23 18:41:22 -0400

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Divya Sasankan on 8/23/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
